ef649158a45ae41ed644c70046e40b1778dc3a81
Alex  Njugi  Karanja <alexnjugi11@gmail.com>
2025-06-26 18:01:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///db.sqlite3")
+    SQLALCHEMY_DATABASE_URI = "postgresql://music_user:tqkBfrBNKZPN3ATdLgKDczKTzq92thMQ@dpg-d1elqommcj7s73eghevg-a/music_player_db_6x7x"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_TOKEN_LOCATION = ['cookies']
