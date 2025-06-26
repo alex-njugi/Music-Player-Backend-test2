50326df71b091e431874fddae8f86f75becbc75d
Alex  Njugi  Karanja <alexnjugi11@gmail.com>
2025-06-26 19:36:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -31,3 +31,13 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+import os
+from models.db import db
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        print("Tables created.")
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
